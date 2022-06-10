14a9710f6194d68c0ba756ad8e4eb133d41e8d7d
Anna Marie Sterling <sterling.am@gmail.com>
2022-06-10 15:44:22 -0600

    Initial commit

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,3 +3,17 @@
 # When you’ve been able to print each row, then see if you can also print the header with the value for each row.
 
 # If you are stuck, reference this page for help: https://backend.turing.edu/module1/lessons/csv_walkthrough
+require 'CSV'
+require './lib/animal_lover'
+# headers: true & header_converter: :symbol are optional arguments
+CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row|
+  id = row[:id].to_i
+  first_name = row[:first_name]
+  last_name = row[:last_name]
+  age = row[:age].to_i
+  animal_lover = AnimalLover.new(id,first_name,last_name,age)
+
+  require 'pry'; binding.pry
+
+  puts "#{animal_lover.full_name} has been created!"
+end
